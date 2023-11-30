56a574e2117d48c09e9b2fb3529a05352579cd69
Harvey Zuccon <hzuccon@gmail.com>
2023-11-30 21:02:05 +1100

    Added time to the model as just the hour field

--- src/backup.js
+++ src/backup.js
@@ -1,109 +1,3 @@
-// import tf from '@tensorflow/tfjs-node'
-// import { data } from '../data.mjs'
-// import fs from 'fs'
-// import normalizeData from './normalizeData.mjs'
-
-// const cleanedData = data.filter(row => row.SOC != null)
-// .filter(row => row.SOC < 99)
-
-// const slicedDataTraining = cleanedData.slice(0, cleanedData.length - (cleanedData.length / 6))
-// const slicedDataTesting = cleanedData.slice(cleanedData.length - (cleanedData.length / 6))
-
-
-
-
-// const trainingLabel = slicedDataTraining.map(({ solarInput }) => Math.floor(solarInput))
-// const trainingData = slicedDataTraining.map(({ solarInput, SOC, time, ...d }) => Object.values({ ...d}))
-// // const trainingData = slicedDataTraining.map(({ solarInput, SOC, time, ...d }) => Object.values({ time: new Date(time).getTime(),  ...d}))
-
-// const testingLabel = slicedDataTesting.map(({ solarInput }) => Math.floor(solarInput))
-// const testingData = slicedDataTesting.map(({ solarInput, SOC, time, ...d }) => Object.values({ ...d}))
-// // const testingData = slicedDataTesting.map(({ solarInput, SOC, time, ...d }) => Object.values({ time: new Date(time).getTime(),  ...d}))
-// console.log(trainingData)
-// console.log(trainingLabel)
-
-// const isEven = testingData.every(d => Object.keys(d).length == 8)
-// console.log(isEven)
-// console.log('matching lengths: ', testingData.length == testingLabel.length)
-
-// // const trainingDataTensor = tf.tensor2d(trainingData)
-// const trainingLabelTensor = tf.tensor2d(trainingLabel, [trainingLabel.length, 1])
-
-// const numTimesteps = 8; // This is an example value
-// const features = 8; // Number of features in your data
-
-// const reshapeData = (data, numTimesteps, features) => {
-//     const reshaped = [];
-//     for (let i = 0; i < data.length - numTimesteps + 1; i++) {
-//         reshaped.push(data.slice(i, i + numTimesteps));
-//     }
-//     return tf.tensor3d(reshaped, [reshaped.length, numTimesteps, features]);
-// };
-
-// const trainingDataTensor = reshapeData(trainingData, numTimesteps, features);
-// const testingDataTensor = reshapeData(testingData, numTimesteps, features);
-
-// const model = tf.sequential()
-
-// model.add(tf.layers.dense({units: 8, activation: 'relu', inputShape: [8]})); // Adjust 'inputShape' based on your feature count
-// model.add(tf.layers.dense({units: 4, activation: 'relu'}));
-// // model.add(tf.layers.dense({units: 32, activation: 'relu'}));
-// // model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-// // model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-// // model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-// // model.add(tf.layers.dense({units: 64, activation: 'relu'}));
-// model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-
-
-// const learningRate = 0.0001; // Set your custom learning rate
-// const optimizer = tf.train.adamax(learningRate);
-
-// model.compile({
-//   optimizer: optimizer,
-//   loss: 'meanSquaredError',
-//   // metrics: ['accuracy']
-// });
-
-// const fileName = new Date().toLocaleString().replace(/\//g, '-').replace(/,/g, '').replace(/:/g, '-').replace(/ /g, '_').replace(' ', '_');
-// fs.mkdirSync(`./models/${fileName}`);
-
-// async function trainModel() {
-//   const response = await model.fit(trainingDataTensor, trainingLabelTensor, {
-//       epochs: 200, // Number of iterations over the entire dataset
-//       validationSplit: 0.2, // Split some data for validation (e.g., 20%)
-//       callbacks: tf.node.tensorBoard('../tmp/fit_logs_1') // Optional: for TensorBoard logging
-//   });
-//   console.log(response.history);
-//   fs.writeFileSync(`./models/${fileName}/history.json`, JSON.stringify(response.history))
-//   if (fs.existsSync('./history.json')) fs.rmSync(`./history.json`)
-//   fs.writeFileSync(`./history.json`, JSON.stringify(response.history))
-// }
-
-// // const testingDataTensor = tf.tensor2d(testingData)
-// const testingLabelTensor = tf.tensor2d(testingLabel, [testingLabel.length, 1])
-
-// trainModel().then(async () => {
-//   // Assuming testDataTensor and testLabelTensor are defined
-//   const evaluation = await model.evaluate(testingDataTensor, testingLabelTensor);
-//   console.log('Model evaluation:', evaluation);
-
-//   if (evaluation[1]) {
-//       console.log('Accuracy', evaluation[1].dataSync()[0]);
-//       console.log('Loss', evaluation[0].dataSync()[0])
-//   } else {
-//     const loss = evaluation.dataSync()[0];
-//     console.log('Test Loss:', loss);
-//   }
-
-
-//   // save the model
-//   // const fileName = new Date().toLocaleString().replace(/\//g, '-').replace(/,/g, '').replace(/:/g, '-').replace(/ /g, '_').replace(' ', '_');
-//   // await fs.mkdirSync('./models');
-//   // await fs.mkdirSync(`./models/${fileName}`);
-//   await model.save(`file://./models/${fileName}`);
-// });
-
-
 import tf from '@tensorflow/tfjs-node';
 import { data } from '../data.mjs';
 import fs from 'fs';
@@ -115,7 +9,14 @@ const fileName = new Date().toLocaleString().replace(/\//g, '-').replace(/,/g, '
 if (fs.existsSync('./history.json')) fs.rmSync(`./history.json`)
 
 // Data Preparation
-const cleanedData = normalizeData(data.filter(row => row.SOC != null && row.SOC < 95), 'solarInput');
+const cleanedData = normalizeData(data.filter(row => row.SOC != null && row.SOC < 95), 'solarInput')
+.map(({ time, ...d }) => {
+    const date = new Date(time);
+    return {
+        time: date.getHours(),
+        ...d
+    }
+})
 
 console.log(cleanedData)
 
@@ -124,11 +25,11 @@ const slicedDataTraining = cleanedData.slice(0, trainSize);
 const slicedDataTesting = cleanedData.slice(trainSize);
 
 const trainingLabel = slicedDataTraining.map(({ solarInput }) => Math.floor(solarInput));
-const trainingData = slicedDataTraining.map(({ solarInput, SOC, time, visibility, ...d }) => Object.values(d));
+const trainingData = slicedDataTraining.map(({ solarInput, SOC, visibility, ...d }) => Object.values(d));
 const testingLabel = slicedDataTesting.map(({ solarInput }) => Math.floor(solarInput));
-const testingData = slicedDataTesting.map(({ solarInput, SOC, time, visibility, ...d }) => Object.values(d));
+const testingData = slicedDataTesting.map(({ solarInput, SOC, visibility, ...d }) => Object.values(d));
 
-const features = 7;
+const features = 8;
 
 const trainingDataTensor = tf.tensor2d(trainingData, [trainingData.length, features]);
 const testingDataTensor = tf.tensor2d(testingData, [testingData.length, features]);
@@ -139,11 +40,11 @@ const testingLabelTensor = tf.tensor2d(testingLabel, [testingLabel.length, 1]);
 // Model Creation
 const model = tf.sequential();
 
-model.add(tf.layers.dense({units: 8, activation: 'relu', inputShape: [features]})); // Adjust 'inputShape' based on your feature count
-model.add(tf.layers.dense({units: 4, activation: 'relu'}));
+model.add(tf.layers.dense({units: 16, activation: 'relu', inputShape: [features]})); // Adjust 'inputShape' based on your feature count
+model.add(tf.layers.dense({units: 16, activation: 'relu'}));
 model.add(tf.layers.dense({units: 1, activation: 'relu'}));
 
-const learningRate = 0.01;
+const learningRate = 0.1;
 const optimizer = tf.train.adamax(learningRate);
 
 model.compile({
@@ -154,8 +55,8 @@ model.compile({
 // Training the Model
 const trainModel = async () => {
     const response = await model.fit(trainingDataTensor, trainingLabelTensor, {
-        epochs: 2500,
-        validationSplit: 0.2,
+        epochs: 400,
+        validationSplit: 0.3,
         callbacks: tf.node.tensorBoard(`../tmp/${fileName}`)
     });
     console.log(response.history);
--- src/loadModel.js
+++ src/loadModel.js
@@ -3,63 +3,41 @@ import normalizeData from './normalizeData.mjs';
 
 const d = [
   {
-    "time": "2023-11-30T00:00",
-    "temperature": 20.7,
-    "cloudCoverage": 76,
+    "time": "2023-12-01T00:00",
+    "temperature": 19.6,
+    "cloudCoverage": 100,
     "solar": 0,
-    "precipitation_probability": 83,
+    "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
     "uv_index": 0
   },
   {
-    "time": "2023-11-30T01:00",
-    "temperature": 20.1,
-    "cloudCoverage": 70,
+    "time": "2023-12-01T01:00",
+    "temperature": 18.8,
+    "cloudCoverage": 100,
     "solar": 0,
-    "precipitation_probability": 69,
+    "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
     "uv_index": 0
   },
   {
-    "time": "2023-11-30T02:00",
-    "temperature": 19.9,
-    "cloudCoverage": 59,
+    "time": "2023-12-01T02:00",
+    "temperature": 18.2,
+    "cloudCoverage": 100,
     "solar": 0,
-    "precipitation_probability": 55,
+    "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
     "uv_index": 0
   },
   {
-    "time": "2023-11-30T03:00",
-    "temperature": 19.7,
-    "cloudCoverage": 56,
-    "solar": 0,
-    "precipitation_probability": 37,
-    "rain": 0,
-    "showers": 0,
-    "visibility": 24140,
-    "uv_index": 0
-  },
-  {
-    "time": "2023-11-30T04:00",
-    "temperature": 19.2,
-    "cloudCoverage": 41,
-    "solar": 0,
-    "precipitation_probability": 18,
-    "rain": 0,
-    "showers": 0,
-    "visibility": 24140,
-    "uv_index": 0
-  },
-  {
-    "time": "2023-11-30T05:00",
-    "temperature": 18.5,
+    "time": "2023-12-01T03:00",
+    "temperature": 17.7,
     "cloudCoverage": 31,
     "solar": 0,
     "precipitation_probability": 0,
@@ -69,9 +47,31 @@ const d = [
     "uv_index": 0
   },
   {
-    "time": "2023-11-30T06:00",
-    "temperature": 18,
-    "cloudCoverage": 0,
+    "time": "2023-12-01T04:00",
+    "temperature": 17.3,
+    "cloudCoverage": 2,
+    "solar": 0,
+    "precipitation_probability": 0,
+    "rain": 0,
+    "showers": 0,
+    "visibility": 24140,
+    "uv_index": 0
+  },
+  {
+    "time": "2023-12-01T05:00",
+    "temperature": 16.9,
+    "cloudCoverage": 5,
+    "solar": 0,
+    "precipitation_probability": 0,
+    "rain": 0,
+    "showers": 0,
+    "visibility": 24140,
+    "uv_index": 0
+  },
+  {
+    "time": "2023-12-01T06:00",
+    "temperature": 16.4,
+    "cloudCoverage": 9,
     "solar": 2,
     "precipitation_probability": 0,
     "rain": 0,
@@ -80,54 +80,54 @@ const d = [
     "uv_index": 0
   },
   {
-    "time": "2023-11-30T07:00",
-    "temperature": 19.3,
-    "cloudCoverage": 0,
-    "solar": 86,
+    "time": "2023-12-01T07:00",
+    "temperature": 17.6,
+    "cloudCoverage": 7,
+    "solar": 77,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 0.6
+    "uv_index": 0.3
   },
   {
-    "time": "2023-11-30T08:00",
-    "temperature": 22,
-    "cloudCoverage": 0,
-    "solar": 284,
+    "time": "2023-12-01T08:00",
+    "temperature": 21.7,
+    "cloudCoverage": 1,
+    "solar": 277,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 2.05
+    "uv_index": 1.75
   },
   {
-    "time": "2023-11-30T09:00",
-    "temperature": 23.6,
+    "time": "2023-12-01T09:00",
+    "temperature": 24.3,
     "cloudCoverage": 0,
-    "solar": 499,
+    "solar": 492,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 3.95
+    "uv_index": 3.9
   },
   {
-    "time": "2023-11-30T10:00",
-    "temperature": 24.5,
+    "time": "2023-12-01T10:00",
+    "temperature": 26.6,
     "cloudCoverage": 0,
-    "solar": 691,
+    "solar": 687,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 5.95
+    "uv_index": 5.9
   },
   {
-    "time": "2023-11-30T11:00",
-    "temperature": 26.1,
-    "cloudCoverage": 5,
-    "solar": 844,
+    "time": "2023-12-01T11:00",
+    "temperature": 28.4,
+    "cloudCoverage": 0,
+    "solar": 840,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
@@ -135,10 +135,10 @@ const d = [
     "uv_index": 7.65
   },
   {
-    "time": "2023-11-30T12:00",
-    "temperature": 27.9,
-    "cloudCoverage": 10,
-    "solar": 941,
+    "time": "2023-12-01T12:00",
+    "temperature": 29.9,
+    "cloudCoverage": 0,
+    "solar": 935,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
@@ -146,10 +146,10 @@ const d = [
     "uv_index": 8.8
   },
   {
-    "time": "2023-11-30T13:00",
-    "temperature": 29.1,
-    "cloudCoverage": 31,
-    "solar": 951,
+    "time": "2023-12-01T13:00",
+    "temperature": 31,
+    "cloudCoverage": 69,
+    "solar": 968,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
@@ -157,10 +157,10 @@ const d = [
     "uv_index": 9.25
   },
   {
-    "time": "2023-11-30T14:00",
-    "temperature": 29.8,
-    "cloudCoverage": 57,
-    "solar": 846,
+    "time": "2023-12-01T14:00",
+    "temperature": 30.4,
+    "cloudCoverage": 5,
+    "solar": 922,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
@@ -168,99 +168,99 @@ const d = [
     "uv_index": 8.95
   },
   {
-    "time": "2023-11-30T15:00",
-    "temperature": 29.9,
-    "cloudCoverage": 46,
-    "solar": 624,
+    "time": "2023-12-01T15:00",
+    "temperature": 29.4,
+    "cloudCoverage": 42,
+    "solar": 801,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 7.95
+    "uv_index": 7.85
   },
   {
-    "time": "2023-11-30T16:00",
-    "temperature": 30,
-    "cloudCoverage": 78,
-    "solar": 514,
+    "time": "2023-12-01T16:00",
+    "temperature": 28.4,
+    "cloudCoverage": 42,
+    "solar": 596,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 6.35
+    "uv_index": 2.3
   },
   {
-    "time": "2023-11-30T17:00",
-    "temperature": 29.5,
-    "cloudCoverage": 52,
-    "solar": 366,
+    "time": "2023-12-01T17:00",
+    "temperature": 27,
+    "cloudCoverage": 58,
+    "solar": 345,
     "precipitation_probability": 0,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 4.4
+    "uv_index": 1.25
   },
   {
-    "time": "2023-11-30T18:00",
-    "temperature": 28.1,
-    "cloudCoverage": 68,
-    "solar": 254,
-    "precipitation_probability": 0,
+    "time": "2023-12-01T18:00",
+    "temperature": 25.3,
+    "cloudCoverage": 100,
+    "solar": 104,
+    "precipitation_probability": 1,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 2.45
+    "uv_index": 2.05
   },
   {
-    "time": "2023-11-30T19:00",
-    "temperature": 25.6,
-    "cloudCoverage": 33,
-    "solar": 98,
-    "precipitation_probability": 0,
+    "time": "2023-12-01T19:00",
+    "temperature": 24.1,
+    "cloudCoverage": 97,
+    "solar": 3,
+    "precipitation_probability": 2,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
-    "uv_index": 0.85
+    "uv_index": 0.45
   },
   {
-    "time": "2023-11-30T20:00",
-    "temperature": 23.3,
-    "cloudCoverage": 40,
-    "solar": 5,
-    "precipitation_probability": 0,
-    "rain": 0,
-    "showers": 0,
+    "time": "2023-12-01T20:00",
+    "temperature": 22.8,
+    "cloudCoverage": 100,
+    "solar": 1,
+    "precipitation_probability": 3,
+    "rain": 0.2,
+    "showers": 0.2,
     "visibility": 24140,
     "uv_index": 0.05
   },
   {
-    "time": "2023-11-30T21:00",
-    "temperature": 22.5,
-    "cloudCoverage": 7,
-    "solar": 0,
-    "precipitation_probability": 0,
-    "rain": 0,
-    "showers": 0,
-    "visibility": 24140,
-    "uv_index": 0
-  },
-  {
-    "time": "2023-11-30T22:00",
-    "temperature": 21.3,
-    "cloudCoverage": 0,
-    "solar": 0,
-    "precipitation_probability": 0,
-    "rain": 0,
-    "showers": 0,
-    "visibility": 24140,
-    "uv_index": 0
-  },
-  {
-    "time": "2023-11-30T23:00",
-    "temperature": 20.3,
+    "time": "2023-12-01T21:00",
+    "temperature": 21.8,
     "cloudCoverage": 100,
     "solar": 0,
-    "precipitation_probability": 0,
+    "precipitation_probability": 6,
+    "rain": 0.8,
+    "showers": 0,
+    "visibility": 24140,
+    "uv_index": 0
+  },
+  {
+    "time": "2023-12-01T22:00",
+    "temperature": 21.2,
+    "cloudCoverage": 100,
+    "solar": 0,
+    "precipitation_probability": 10,
+    "rain": 0,
+    "showers": 0,
+    "visibility": 24140,
+    "uv_index": 0
+  },
+  {
+    "time": "2023-12-01T23:00",
+    "temperature": 20.4,
+    "cloudCoverage": 74,
+    "solar": 0,
+    "precipitation_probability": 13,
     "rain": 0,
     "showers": 0,
     "visibility": 24140,
@@ -269,14 +269,14 @@ const d = [
 ]
 
 const loadModel = async () => {
-    const model = await tf.loadLayersModel('file://./testModel/a3/model.json');
+    const model = await tf.loadLayersModel('file://./testModel/a4/model.json');
     model.predict
     return model;
 }
 
 const predict = async (model, data) => {
   // const cleaned = normalise(data);
-  const arrayed = data.map(({ solarInput, SOC, visibility, time, ...d }) => Object.values(d))
+  const arrayed = data.map(({ solarInput, SOC, time, visibility, ...d }) => Object.values(d))
   const cleaned = reshape(arrayed);
   console.log(cleaned)
 
@@ -296,9 +296,11 @@ const reshape = (data) => {
 }
 
 const normalise = (data) => {
-  const normalised = normalizeData(data, 'solarInput');
+  const normalised = normalizeData(data, 'solarInput')
+  
   console.log(normalised)
-  const cleaned = normalised.map(({ solarInput, SOC, visibility, time, ...d }) => Object.values(d))
+  
+  const cleaned = normalised.map(({ solarInput, SOC, time, visibility, ...d }) => Object.values(d))
   const reshaped = [];
   for (let i = 0; i <= cleaned.length - 24; i++) {
       reshaped.push(cleaned.slice(i, i + 24));
@@ -334,16 +336,18 @@ main();
 
 
 const dense = async (model, data) => {
+  data = data.map(({ time, ...d }) => ({ time: new Date(time).getHours(), ...d  }))
+
   const normalised = normalizeData(data, 'solarInput');
   console.log(normalised)
-  const cleaned = normalised.map(({ solarInput, SOC, visibility, time, ...d }) => Object.values(d))
+  const cleaned = normalised.map(({ solarInput, SOC, visibility, ...d }) => Object.values(d))
   const nanRemoved = cleaned.map(row => row.map(v => isNaN(v) ? 0 : v))
   console.log(nanRemoved)
 
   let prediction
   for (let i = 0; i < nanRemoved.length; i++) {
     const row = nanRemoved[i];
-    const tensor = tf.tensor2d(row, [1, 7]);
+    const tensor = tf.tensor2d(row, [1, 8]);
   
     prediction = await model.predict(tensor);
     console.log(normalised[i].time, prediction.dataSync()[0]);
